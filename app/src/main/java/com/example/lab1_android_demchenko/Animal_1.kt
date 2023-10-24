91e32baa9e4c25d97e75ef4dc8b039b83118e85b
VitalyDemchenko <v.demchenko.ns@gmail.com>
2023-10-24 21:43:02 +0300

    add 3 layouts ч3

--- app/src/main/java/com/example/lab1_android_demchenko/Animal_1.kt
+++ app/src/main/java/com/example/lab1_android_demchenko/Animal_1.kt
@@ -2,10 +2,31 @@ package com.example.lab1_android_demchenko
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.content.Intent
+import androidx.databinding.DataBindingUtil
+import com.example.lab1_android_demchenko.databinding.ActivityAnimal1Binding
 
 class Animal_1 : AppCompatActivity() {
+    private lateinit var binding : ActivityAnimal1Binding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_animal1)
+
+        binding = ActivityAnimal1Binding.inflate(layoutInflater)
+        val contentView = binding.root
+        setContentView(contentView)
+        // Встановлюємо значення h для txtopdog TextView через binding об'єкт
+
+        binding.txupdog2.text = dog2.txtopdog2
+        binding.txdowndog2.text = dog2.txdowndog2
+        binding.txbigdog2.text = dog2.extended_text_dog2
+       // setContentView(R.layout.activity_animal1)
+
     }
+
+
+   var dog2 = dog()
+
+
 }
--- app/src/main/java/com/example/lab1_android_demchenko/MainActivity.kt
+++ app/src/main/java/com/example/lab1_android_demchenko/MainActivity.kt
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.content.Intent
-//import androidx.databinding.DataBindingUtil
+import androidx.databinding.DataBindingUtil
 import com.example.lab1_android_demchenko.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -13,13 +13,24 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+       // setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val contentView = binding.root
+        setContentView(contentView)
+        // Встановлюємо значення h для txtopdog TextView через binding об'єкт
 
-
+        binding.txtopdog.text = dog1.txtopdog2
+        binding.txdowndog.text = dog1.txdowndog2
 
     }
-    val dog1 = dog()
-        // txtopog.text
+    var dog1 = dog()
+        // val h = dog1.txtopdog2
+
+
+
+
+
+
 fun goAn1(v: View)
 {
     startActivity(Intent(this, Animal_1::class.java))
